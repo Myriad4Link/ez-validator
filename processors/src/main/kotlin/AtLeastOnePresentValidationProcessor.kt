cc24649fcfa9702c6275c477badf11e7f16de3fb
影粒 <2661734285@qq.com>
2025-12-23 12:33:16 +0800

    feat: Enhance AtLeastOnePresentValidationProcessor to support factory function generation with error handling

--- processors/src/main/kotlin/AtLeastOnePresentValidationProcessor.kt
+++ processors/src/main/kotlin/AtLeastOnePresentValidationProcessor.kt
@@ -1,38 +1,55 @@
+import arrow.core.Either
 import arrow.core.Option
-import com.google.devtools.ksp.processing.CodeGenerator
-import com.google.devtools.ksp.processing.KSPLogger
-import com.google.devtools.ksp.processing.Resolver
-import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.*
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSFile
 import com.google.devtools.ksp.symbol.KSValueParameter
-import com.squareup.kotlinpoet.FunSpec
-import com.squareup.kotlinpoet.ParameterSpec
+import com.squareup.kotlinpoet.*
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.ksp.toClassName
 import com.squareup.kotlinpoet.ksp.toTypeName
+import com.squareup.kotlinpoet.ksp.writeTo
 
 class AtLeastOnePresentValidationProcessor(
     private val codeGenerator: CodeGenerator,
-    private val logger: KSPLogger
+    private val logger: KSPLogger,
+    private val options: Map<String, String>
 ) : SymbolProcessor {
     override fun process(resolver: Resolver): List<KSAnnotated> {
         val symbols = resolver.getSymbolsWithAnnotation(AtLeastOnePresent::class.qualifiedName!!)
             .filterIsInstance<KSClassDeclaration>()
 
-        symbols.forEach { this.generateFactoryFunction(it) }
-        TODO("Not yet implemented")
+        val sourceFiles = ArrayList<KSFile>()
+        val fileSpecBuilder = FileSpec.builder(
+            options["ez-validator.package"] ?: "xyz.uthofficial.ksp.ezvalidator",
+            options["ez-validator.factoryName"] ?: "ValidatedClassFactory"
+        )
+
+        symbols.forEach {
+            it.containingFile?.let { sourceFile -> sourceFiles.add(sourceFile) }
+            createFactorySpec(it)
+                .onFailure { e -> logger.error(e.message!!, it) }
+                .onSuccess(fileSpecBuilder::addFunction)
+        }
+
+        fileSpecBuilder.build()
+            .writeTo(
+                codeGenerator,
+                Dependencies(true, *sourceFiles.toTypedArray())
+            )
+
+        return emptyList()
     }
 
-    private fun generateFactoryFunction(classDeclaration: KSClassDeclaration) {
-        val packageName = classDeclaration.packageName.asString()
+    private fun createFactorySpec(classDeclaration: KSClassDeclaration): Result<FunSpec> {
         val className = classDeclaration.simpleName.asString()
 
         val constructor = classDeclaration.primaryConstructor
 
         when {
             constructor == null -> {
-                logger.error("Class $className must have a primary constructor to validate with", classDeclaration)
-                return
+                return Result.failure(IllegalArgumentException("Class $className must have a primary constructor to validate with"))
             }
         }
 
@@ -44,15 +61,42 @@ class AtLeastOnePresentValidationProcessor(
             else -> "true"
         }
 
-        val constructorCallArgs = parameterSpecs.joinToString(", ") { it.name }
+        val constructorCallArgsBlock = parameterSpecs.map {
+            CodeBlock.of("""%N = %N""", it, it)
+        }.joinToCode(", ")
 
-        val factoryFunction = FunSpec.builder("create$className")
-            // Since it is a factory function where failure is expected, we return EITHER a validation error or the
-            // successfully created one.
-            .returns(classDeclaration.toClassName())
-            // Add the parameters extracted from the original one.
-            .addParameters(parameterSpecs)
-            .addStatement("val isAnyPresent = $conditionCode")
+        return Result.success(
+            FunSpec.builder("create$className")
+                // Since it is a factory function where failure is expected, we return EITHER a validation error or the
+                // successfully created one.
+                .returns(
+                    Either::class.asClassName()
+                        .parameterizedBy(
+                            TooLessPresentedError::class.asClassName(),
+                            classDeclaration.toClassName()
+                        )
+                )
+                // Add the parameters extracted from the original one.
+                .addParameters(parameterSpecs)
+                .addStatement("val isAnyPresent = $conditionCode")
+                .beginControlFlow("if (!isAnySet)")
+                // Expected (at least) 1, found 0.
+                .addStatement(
+                    """return %T.Left(%T(%L, %L))""",
+                    Either::class.asClassName(),
+                    TooLessPresentedError::class.asClassName(),
+                    1,
+                    0
+                )
+                .endControlFlow()
+                .addStatement(
+                    """return %T.Right(%T(%L))""",
+                    Either::class.asClassName(),
+                    classDeclaration.toClassName(),
+                    constructorCallArgsBlock
+                )
+                .build()
+        )
     }
 
     private fun List<KSValueParameter>.toPoetParameterSpecs(): List<ParameterSpec> = this.map {
